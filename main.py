847b700698f5a294754d5bfdce802f6ec9fe6e08
MidasO <149903808+MidasO@users.noreply.github.com>
2023-11-04 21:08:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import json
 
+def lire_fichier(chemin_du_fichier):
+    """Extrait et retourne le contenu du fichier json "chemin_du_fichier"."""
+    with open(chemin_du_fichier, 'r', encoding='utf-8') as f_in:
+        return json.load(f_in)
+
+def construire_mapping(personnes):
+    """Retourne un dictionnaire avec pour clés les noms des personnes et pour
+    valeurs les noms de leurs enfants sur base de la liste de dictionnaires
+    "personnes"."""
+    dico = {}
+    for i in len(personnes):
+        dico[personnes[i]["nom"]] = personnes[i]["enfants"]
+    return dico
+
+
+
+dico = construire_mapping(lire_fichier())
