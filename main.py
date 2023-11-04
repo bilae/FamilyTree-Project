67dc7752645965c0dce5a2d9b7aadef72efd7786
PloZx <57329223+PloZx@users.noreply.github.com>
2023-11-04 22:42:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,44 @@ def construire_mapping(personnes):
     valeurs les noms de leurs enfants sur base de la liste de dictionnaires
     "personnes"."""
     mapping = {}
-    for i in range(len(personnes)):
-        mapping[personnes[i]["nom"]] = personnes[i]["enfants"]
+    for personne in personnes:
+        nom = personne["nom"]
+        enfants = personne.get("enfants", [])
+        mapping[nom] = enfants
     return mapping
+
+# Charge le fichier JSON
+data = lire_fichier("chemin_du_fichier.json")
+
+# Construit le mapping
+mapping = construire_mapping(data)
+
+def compter_descendants_et_profondeur(nom, mapping, cache_compte={}, cache_profondeur={}, generation_courante=1):
+
+    total_descendants = 0
+    profondeur = generation_courante
+
+    for enfant in mapping.get(nom, []):
+        result = compter_descendants_et_profondeur(enfant, mapping, cache_compte, cache_profondeur, generation_courante + 1)
+        total_descendants += 1 + result["total_descendants"]
+        if result["generations"] > profondeur:
+            profondeur = result["generations"]
+
+    cache_compte[nom] = total_descendants
+    cache_profondeur[nom] = profondeur
+
+    return {"nom": nom, "total_descendants": total_descendants, "generations": profondeur}
+
+# Liste de résultats
+resultats = []
+
+for nom in mapping.keys():
+    resultats.append(compter_descendants_et_profondeur(nom, mapping))
+
+# Sauvegarde les résultats dans un fichier JSON
+with open("resultats.json", "w") as fichier_sortie:
+    json.dump(resultats, fichier_sortie, indent=4)
+
 
 def trier_par_selection(personnes, nb_desc_ou_gen):
     """Trie personnes par nombre de descendants quand nb_desc_ou_gen vaut
